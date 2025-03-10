fae87a264d8bcf37a892bd95b1bf50f15e1adcc4
Danmark12 <ihopeyougetbanzaid@gmail.com>
2025-03-10 11:28:59 +0800

    added Palindrome

---
+++ Madria_IT13B_activities/src/Midterm/Palindrome.java
@@ -0,0 +1,30 @@
+
+package Midterm;
+
+import java.util.Scanner;
+
+
+public class Palindrome {
+    
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = in.nextLine();
+
+        String reversed = "";
+
+        for (int x = word.length() - 1; x >= 0; x--) {
+            reversed += word.charAt(x);
+        }
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println(word.toUpperCase() + " is a Palindrome");
+        } else {
+            System.out.println(word.toUpperCase() + " is not a Palindrome");
+        }
+    }
+
+}
+
+                
