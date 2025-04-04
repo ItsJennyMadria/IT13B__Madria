8fa22a4b33434248875d51ab231243e234f767c6
Jenny Madria <ihopeyougetbanzaid@gmail.com>
2025-04-04 21:59:27 +0800

    Added_Fibonacci_Act

---
+++ Madria_IT13B_activities/src/Midterm/Fibonacci_Activity1.java
@@ -0,0 +1,23 @@
+
+package Midterm;
+
+
+public class Fibonacci_Activity1 {
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.print("The Fibonacci Sequence of " + n + " are: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " , ");
+
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
+
